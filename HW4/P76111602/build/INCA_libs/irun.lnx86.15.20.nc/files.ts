dca3a7f487930122064989887e4117476e01bab8
YuChengWang <brian0978599918@gmail.com>
2022-12-21 23:48:18 +0800

    HW4 vol.3

--- HW4/P76111602/build/INCA_libs/irun.lnx86.15.20.nc/files.ts
+++
@@ -1,1 +0,0 @@
-1671095442 /home/YuChengWang/VLSI-System-Design/HW4/P76111602/sim/top_tb.sv
--- HW4/P76111602/build/INCA_libs/irun.lnx8664.15.20.nc/.timestamp.ts
+++
@@ -1,2 +0,0 @@
-1671444530 /home/YuChengWang/VLSI-System-Design/HW4/P76111602/sim/top_tb.sv
-1671444530 /home/YuChengWang/VLSI-System-Design/HW4/P76111602/sim/top_tb_WDT.sv
